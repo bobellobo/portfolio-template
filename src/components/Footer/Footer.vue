a5a54a6f5c21b75798a0a6266dcc4211cecc216e
bobellobo <titouanguedon@proton.me>
2026-03-13 01:27:28 +0100

    add repo in footer credit

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -2,6 +2,19 @@
   <footer class="footer">
     <div class="container">
       <p>{{ $t('footer.copyright') }}</p>
+      <p>
+        <i18n-t keypath="footer.templateCredit" tag="span">
+          <template #link>
+            <a
+              :href="templateRepositoryUrl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {{ $t('footer.templateCreditLabel') }}
+            </a>
+          </template>
+        </i18n-t>
+      </p>
     </div>
   </footer>
 </template>
@@ -10,6 +23,8 @@
 import { useI18n } from 'vue-i18n'
 
 useI18n()
+
+const templateRepositoryUrl = 'https://github.com/bobellobo/portfolio-scaffolder'
 </script>
 
 <style scoped src="./Footer.css"></style>
